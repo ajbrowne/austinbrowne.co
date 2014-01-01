5a638b91cefa4425a89891a20dbdc4b4796a159d
Austin Browne <dazztrazak@gmail.com>
2014-01-01 15:09:18 -0500

    Yup

--- MTG/results.js
+++ MTG/results.js
@@ -4,16 +4,13 @@ var price = 'http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname
 var low;
 var med;
 var high;
-var one, two, three, four, five, six, seven, eight, nine, ten, eleven; 
-var url;
+var json = 'https://dl.dropboxusercontent.com/s/4o2dhek26eis1vj/AllSets-x.json'
 
 function cardSearch(){
   var input = sessionStorage.getItem('card');
   var l_input = sessionStorage.getItem('card').toLowerCase();
-  $.getJSON('AllSets-x.json', function (data) {
-    alert('gogo');
+  $.getJSON(json, function (data) {
     $.each(data, function (index, value){
-      alert('bloblo');
      var cards = data[index].cards;
      $.each(cards, function (ind, value){
       var name = cards[ind].name;
@@ -24,7 +21,7 @@ function cardSearch(){
         for (var i = 1; i < cards[ind].printings.length; i++){
           printings += '/' + cards[ind].printings[i];
         }
-        url = "http://mtgimage.com/card/";
+        var url = "http://mtgimage.com/card/";
         var img = input.replace(' ', '%20');
         var buy = input.replace(' ', '+');
         tcg += buy;
@@ -38,51 +35,52 @@ function cardSearch(){
         img = img + '.jpg';
         url = url + img;
         var pt = cards[ind].power + '/' + cards[ind].toughness;
-        one = url;
-        two = cards[ind].name;
-        three = cards[ind].name;
-        four = type;
-        five = cards[ind].text;
-        six = printings;
-        seven = cards[ind].rarity;
-        eight = cards[ind].artist;
+        document.getElementById('pic').src = url;
+        document.getElementById('name').innerHTML += cards[ind].name;
+        document.getElementById('card name').innerHTML += cards[ind].name;
+        document.getElementById('type').innerHTML += type;
+        document.getElementById('ctext').innerHTML += cards[ind].text
+        document.getElementById('expac').innerHTML += printings;
+        document.getElementById('rarity').innerHTML += cards[ind].rarity;
+        document.getElementById('artist').innerHTML += cards[ind].artist;
+        document.getElementById('buy').href = tcg;
+        document.getElementById('pic').style.visibility = 'visible';
+        document.getElementById('name').style.visibility = 'visible';
+        document.getElementById('card name').style.visibility = 'visible';
+        document.getElementById('type').style.visibility = 'visible';
+        document.getElementById('ctext').style.visibility = 'visible';
+        document.getElementById('flavor').style.visibility = 'visible';
+        document.getElementById('expac').style.visibility = 'visible';
+        document.getElementById('rarity').style.visibility = 'visible';
+        document.getElementById('artist').style.visibility = 'visible';
         if (cards[ind].flavor != null){
          var flavor = cards[ind].flavor;
          flavor = flavor.italics(); 
-         nine = flavor;
+         document.getElementById('flavor').innerHTML += flavor;
+         document.getElementById('flavor').style.visibility = 'visible';
        } else{
         document.getElementById('flavor').remove();
       }
       if (cards[ind].cmc != null){
-        ten = cards[ind].cmc;
-      } else{
-        document.getElementById('cmc').remove();
-      }
-      if (type.indexOf(creature) != -1){
-        eleven = pt;
-      } else{
-        document.getElementById('pt').remove();
-      }
-      input = 'wallawalla';
+       document.getElementById('cmc').innerHTML += cards[ind].cmc;
+       document.getElementById('cmc').style.visibility = 'visible';
+     } else{
+      document.getElementById('cmc').remove();
     }
-  });
-});
-if (check == false){
-  $('#myModal').modal('show');
+    if (type.indexOf(creature) != -1){
+     document.getElementById('pt').innerHTML += pt;
+     document.getElementById('pt').style.visibility = 'visible';
+   } else{
+    document.getElementById('pt').remove();
+  }
+  input = 'wallawalla';
 }
 });
-document.getElementById('pic').src = url;document.getElementById('pic').src = one;
-document.getElementById('name').innerHTML += two;
-document.getElementById('card name').innerHTML += three;
-document.getElementById('type').innerHTML += four;
-document.getElementById('ctext').innerHTML += five;
-document.getElementById('expac').innerHTML += six;
-document.getElementById('rarity').innerHTML += seven;
-document.getElementById('artist').innerHTML += eight;
-document.getElementById('buy').href = price;
-document.getElementById('flavor').innerHTML += nine;
-document.getElementById('cmc').innerHTML += ten;
-document.getElementById('pt').innerHTML += eleven;
+});
+if (check == false){
+    $('#myModal').modal('show');
+  }
+});
 }
 
 function forward(){
